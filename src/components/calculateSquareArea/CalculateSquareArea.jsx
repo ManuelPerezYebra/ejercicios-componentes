dbb3e4f4e6dce29cd92f766e23a0595c5cf78df8
Manuel Perez Yebra <154430764+ManuelPerezYebra@users.noreply.github.com>
2024-05-30 14:26:31 +0200

    Update CalculateSquareArea.jsx

--- src/components/calculateSquareArea/CalculateSquareArea.jsx
+++ src/components/calculateSquareArea/CalculateSquareArea.jsx
@@ -1,5 +1,5 @@
 const CalculateSquareArea = ({distance}) =>{
-    return <h2>El area del triangulo es: {distance*distance} m2</h2>
+    return <h2>El area del cuadrado es: {distance*distance} m2</h2>
 }
 
 export default CalculateSquareArea
